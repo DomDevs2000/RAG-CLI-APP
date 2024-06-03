1dd526f679c4c2ba44821d4cccb1a4b74a4db43b
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-06-03 19:00:53 +0100

    added Rag Request and Response model

---
+++ RAG/src/main/java/com/AidanC/RAG/model/RAGRequest.java
@@ -0,0 +1,13 @@
+package com.AidanC.RAG.model;
+
+public class RAGRequest {
+    private String query;
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+}
---
+++ RAG/src/main/java/com/AidanC/RAG/model/RAGResponse.java
@@ -0,0 +1,17 @@
+package com.AidanC.RAG.model;
+
+public class RAGResponse {
+    private String content;
+
+    public RAGResponse(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
