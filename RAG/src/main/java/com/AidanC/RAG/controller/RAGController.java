44576f9d44ea3bc1a9d26ca461a43a2b5791b6b7
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-06-03 19:34:52 +0100

    updated controller functionality

--- RAG/src/main/java/com/AidanC/RAG/controller/RAGController.java
+++ RAG/src/main/java/com/AidanC/RAG/controller/RAGController.java
@@ -1,15 +1,40 @@
 package com.AidanC.RAG.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.AidanC.RAG.service.RAGService;
+import com.AidanC.RAG.model.RAGResponse;
 
 @RestController
 @RequestMapping("/api/v1")
 public class RAGController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "Hello World!";
+    private final RAGService ragService;
+
+    @Autowired
+    public RAGController(RAGService ragService) {
+        this.ragService = ragService;
+    }
+
+    @GetMapping("/faq")
+    public ResponseEntity<RAGResponse> generateAnswer(
+            @RequestParam(value = "message", defaultValue = "summarise a section about project management") String message) {
+        String responseContent = ragService.getAnswer(message);
+        RAGResponse ragResponse = new RAGResponse(responseContent);
+        return ResponseEntity.ok(ragResponse);
+    }
+
+    @PostMapping("/test")
+    public ResponseEntity<RAGResponse> test(@RequestBody String message) {
+        String responseContent = ragService.getAnswer(message);
+        RAGResponse faqResponse = new RAGResponse(responseContent);
+        return ResponseEntity.ok(faqResponse);
+
     }
 }
