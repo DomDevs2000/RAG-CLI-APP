d99a4c179c7e5437d751ff1aead33c3989b9158a
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-06-03 18:54:22 +0100

    implemented basic vector store config

---
+++ RAG/src/main/java/com/AidanC/RAG/config/RAGConfig.java
@@ -0,0 +1,53 @@
+
+package com.AidanC.RAG.config;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+@Configuration
+public class RAGConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(RAGConfig.class);
+
+    @Value("vectorstore.json")
+    private String vectorStoreName;
+
+    @Value("classpath:/docs/MScProjectHandbook.pdf")
+    private Resource faq;
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder) {
+        return builder.defaultSystem("Test").build();
+    }
+
+    @Bean
+    public SimpleVectorStore simpleVectorStore(EmbeddingModel embeddingClient) throws IOException {
+        SimpleVectorStore simpleVectorStore = new SimpleVectorStore(embeddingClient);
+        File vectorStoreFile = getVectorStoreFile();
+        if (vectorStoreFile.exists()) {
+            log.info("Vector Store File Exists. Loading...");
+            simpleVectorStore.load(vectorStoreFile);
+        } else {
+            log.info("Vector Store File Does Not Exist, Creating...");
+        }
+        return simpleVectorStore;
+    }
+
+    private File getVectorStoreFile() {
+        Path path = Paths.get("src", "main", "resources", "data");
+        String absolutePath = path.toFile().getAbsolutePath() + "/" + vectorStoreName;
+        return new File(absolutePath);
+    }
+}
