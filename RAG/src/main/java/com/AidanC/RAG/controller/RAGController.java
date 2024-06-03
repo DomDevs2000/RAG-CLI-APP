43e4a0014d1009fc1b04a81871bd1f836ce67749
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-06-03 18:48:40 +0100

    implemented basic test controller

---
+++ RAG/src/main/java/com/AidanC/RAG/controller/RAGController.java
@@ -0,0 +1,15 @@
+package com.AidanC.RAG.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class RAGController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello World!";
+    }
+}
