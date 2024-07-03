0bcdf6abc0d34679def7fd494d34026d22cd00fc
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-07-03 21:53:55 +0100

    removed unused import statements

--- RAG/src/main/java/com/AidanC/RAG/RagApplication.java
+++ RAG/src/main/java/com/AidanC/RAG/RagApplication.java
@@ -4,15 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 
 import com.AidanC.RAG.config.PdfFileReaderConfig;
 import com.AidanC.RAG.config.TikaFileReaderConfig;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @SpringBootApplication
 public class RagApplication implements  CommandLineRunner {
--- RAG/src/main/java/com/AidanC/RAG/service/RAGService.java
+++ RAG/src/main/java/com/AidanC/RAG/service/RAGService.java
@@ -3,14 +3,9 @@ package com.AidanC.RAG.service;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.messages.Message;
-import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.PgVectorStore;
 import org.springframework.ai.vectorstore.SearchRequest;
