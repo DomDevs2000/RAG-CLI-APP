cdb378e4526a2018eface886c5acd9a4847956ec
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-07-12 16:17:06 +0100

    organised imports

--- RAG/src/main/java/com/AidanC/RAG/controller/RAGController.java
+++ RAG/src/main/java/com/AidanC/RAG/controller/RAGController.java
@@ -3,13 +3,14 @@ package com.AidanC.RAG.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import com.AidanC.RAG.service.RAGService;
+
 import com.AidanC.RAG.model.RAGResponse;
+import com.AidanC.RAG.service.RAGService;
 
 @RestController
 @RequestMapping("/api/v1")
