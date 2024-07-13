19e95c27db8b99298d1f126881d0789b3d0fbdc5
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-07-13 17:52:12 +0100

    updated url to aws ebs url - added budget command

--- CLI/src/main/java/com/AidanC/CLI/ApiCommand.java
+++ CLI/src/main/java/com/AidanC/CLI/ApiCommand.java
@@ -5,7 +5,6 @@ import org.springframework.shell.command.annotation.Command;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.web.reactive.function.client.WebClient;
 
-
 @Command
 public class ApiCommand {
 
@@ -15,21 +14,27 @@ public class ApiCommand {
         this.webClient = webClientBuilder.build();
     }
 
-    private static final String API_URL = "http://35.177.40.177:8080/api/v1/faq";
-
+    private static final String API_URL = "http://msc-rag-api-28879415.eu-west-2.elb.amazonaws.com/api/v1/budget";
+    private static final String BUDGET_URL = "http://msc-rag-api-28879415.eu-west-2.elb.amazonaws.com/api/v1/budget";
     private static final String TEST_URL = "http://35.177.40.177:8080/api/v1/test";
-// private static final String LOCAL_URL = "http://localhost:8080/api/v1/test";
 
     @Command(command = "faq")
     public String test() {
         return getResponse();
     }
 
+    @Command(command = "budget")
+    public String budget() {
+        ApiResponse response = webClient.get().uri(BUDGET_URL).retrieve()
+                .bodyToMono(ApiResponse.class)
+                .block();
+        return response != null ? response.getContent() : "No response received";
+    }
+
     @Command(command = "test")
     public String example(String message) {
         ApiResponse response = webClient.post()
-                .uri(TEST_URL).
-                contentType(MediaType.APPLICATION_JSON)
+                .uri(TEST_URL).contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(message)
                 .retrieve()
                 .bodyToMono(ApiResponse.class)
@@ -37,13 +42,11 @@ public class ApiCommand {
         return response != null ? response.getContent() : "No response received";
     }
 
-
     @ShellMethod("test-method")
     public String postQuery(String message) {
 
         ApiResponse response = webClient.post()
-                .uri(TEST_URL).
-                contentType(MediaType.APPLICATION_JSON)
+                .uri(TEST_URL).contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(message)
                 .retrieve()
                 .bodyToMono(ApiResponse.class)
@@ -54,12 +57,9 @@ public class ApiCommand {
     @ShellMethod("get-response")
     public String getResponse() {
         ApiResponse response = webClient.get()
-                .uri(API_URL).
-                retrieve()
+                .uri(API_URL).retrieve()
                 .bodyToMono(ApiResponse.class)
                 .block(); // blocking call for simplicity in CLI app
         return response != null ? response.getContent() : "No response received";
     }
 }
-
-
