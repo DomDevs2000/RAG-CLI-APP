89eb734edd75d046b7fe71089b261bfad8d23f6d
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-07-22 16:27:11 +0100

    formatted code - removed unused imports

--- CLI/src/main/java/com/AidanC/CLI/ApiCommand.java
+++ CLI/src/main/java/com/AidanC/CLI/ApiCommand.java
@@ -2,11 +2,9 @@ package com.AidanC.CLI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.shell.command.annotation.Command;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @ShellComponent
