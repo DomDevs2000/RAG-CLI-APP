3a8a4f5573ba057ceffab14552470c406d5a54b3
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-07-22 16:25:43 +0100

    fixed bug not recognising post request body

--- CLI/src/main/java/com/AidanC/CLI/ApiCommand.java
+++ CLI/src/main/java/com/AidanC/CLI/ApiCommand.java
@@ -1,65 +1,49 @@
 package com.AidanC.CLI;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.shell.command.annotation.Command;
+import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@Command
+@ShellComponent
 public class ApiCommand {
 
     private final WebClient webClient;
 
+    @Autowired
     public ApiCommand(WebClient.Builder webClientBuilder) {
         this.webClient = webClientBuilder.build();
     }
 
-    private static final String API_URL = "http://msc-rag-api-28879415.eu-west-2.elb.amazonaws.com/api/v1/budget";
-    private static final String BUDGET_URL = "http://msc-rag-api-28879415.eu-west-2.elb.amazonaws.com/api/v1/budget";
-    private static final String TEST_URL = "http://35.177.40.177:8080/api/v1/test";
+    // private static final String HOSTED_URL =
+    // "http://msc-rag-api-28879415.eu-west-2.elb.amazonaws.com/api/v1/budget";
+    private static final String LOCAL_URL = "http://localhost:8080/api/v1/chat";
 
-    @Command(command = "faq")
-    public String test() {
-        return getResponse();
+    private static final String BUDGET_URL = "http://localhost:8080/api/v1/budget";
+
+    @ShellMethod("Chat Command")
+    public String chat(@ShellOption String message) {
+        ApiResponse response = webClient.post()
+                .uri(LOCAL_URL).contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(message)
+                .retrieve()
+                .bodyToMono(ApiResponse.class)
+                .block();
+        return response != null ? response.getContent() : "No response received";
     }
 
-    @Command(command = "budget")
+    @ShellMethod("Budget Command")
     public String budget() {
-        ApiResponse response = webClient.get().uri(BUDGET_URL).retrieve()
-                .bodyToMono(ApiResponse.class)
-                .block();
-        return response != null ? response.getContent() : "No response received";
-    }
-
-    @Command(command = "test")
-    public String example(String message) {
-        ApiResponse response = webClient.post()
-                .uri(TEST_URL).contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(message)
-                .retrieve()
-                .bodyToMono(ApiResponse.class)
-                .block();
-        return response != null ? response.getContent() : "No response received";
-    }
-
-    @ShellMethod("test-method")
-    public String postQuery(String message) {
-        ApiResponse response = webClient.post()
-                .uri(TEST_URL).contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(message)
-                .retrieve()
-                .bodyToMono(ApiResponse.class)
-                .block();
-        return response != null ? response.getContent() : "No response received";
-
-    }
-
-    @ShellMethod("get-response")
-    public String getResponse() {
         ApiResponse response = webClient.get()
-                .uri(API_URL).retrieve()
+                .uri(BUDGET_URL)
+                .retrieve()
                 .bodyToMono(ApiResponse.class)
                 .block();
         return response != null ? response.getContent() : "No response received";
     }
+
 }
