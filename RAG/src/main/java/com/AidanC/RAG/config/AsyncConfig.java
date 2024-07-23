30f2030635641c64631148ae88700e95bfb6673a
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-07-23 22:30:43 +0100

    updated Async Config

--- RAG/src/main/java/com/AidanC/RAG/config/AsyncConfig.java
+++ RAG/src/main/java/com/AidanC/RAG/config/AsyncConfig.java
@@ -3,19 +3,25 @@ package com.AidanC.RAG.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
 @Configuration
+@EnableAsync
 public class AsyncConfig implements AsyncConfigurer {
 
   @Bean(name = "Executor")
   public Executor taskExecutor() {
+    int coreCount = Runtime.getRuntime().availableProcessors();
+    int threadCount = coreCount * 2;
+    System.out.println(coreCount);
+    System.out.println(threadCount);
     ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-    executor.setCorePoolSize(5);
-    executor.setMaxPoolSize(10);
-    executor.setQueueCapacity(25);
+    executor.setCorePoolSize(threadCount);
+    executor.setMaxPoolSize(threadCount);
+    executor.setQueueCapacity(500);
     executor.setThreadNamePrefix("Async-");
     executor.initialize();
     return executor;
