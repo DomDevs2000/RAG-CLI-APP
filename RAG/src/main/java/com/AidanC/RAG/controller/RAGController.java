7b13291ab9114d7e2e26741ac1d77f68f0ebc851
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-07-24 22:58:43 +0100

    organised imports

--- RAG/src/main/java/com/AidanC/RAG/controller/RAGController.java
+++ RAG/src/main/java/com/AidanC/RAG/controller/RAGController.java
@@ -1,18 +1,14 @@
 package com.AidanC.RAG.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.AidanC.RAG.config.PdfFileReaderConfig;
