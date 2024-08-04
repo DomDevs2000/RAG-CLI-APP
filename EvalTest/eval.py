604edecbd4c14ed5ddfb8d5c77b2725fb99090db
DomDevs <aidan@carvalho.org.uk>
2024-08-04 18:37:03 +0100

    updated eval tests

--- EvalTest/eval.py
+++ EvalTest/eval.py
@@ -1,17 +1,30 @@
 from datasets import Dataset
 import os
 import pandas as pd
+import requests
 from ragas import evaluate
 from ragas.metrics import faithfulness, answer_correctness, context_precision, context_recall, answer_relevancy, \
     context_entity_recall, answer_similarity
 from ragas.metrics.critique import harmfulness
+questions =[
+    'When was the first super bowl?',
+    'Who won the most super bowls?'
+]
+url = "http://localhost:8080/api/v1/chat"
 
-# os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
-
+payload = {"message": questions}
+response = requests.post(url, json=payload)
+# answer = response.text
+answer = "The first superbowl was held on January 15, 1967."
+answers = []
+for question in questions:
+    payload = {"message": question}
+    response = requests.post(url, json=payload)
+    answer = response.text
+    answers.append(answer)
 data_samples = {
-    'question': ['When was the first super bowl?', 'Who won the most super bowls?'],
-    'answer': ['The first superbowl was held on Jan 15, 1967',
-               'The most super bowls have been won by The New England Patriots'],
+    'question': questions,
+    'answer': answers,
     'contexts': [[
                      'The First AFL–NFL World Championship Game was an American football game played on January 15, 1967, at the Los Angeles Memorial Coliseum in Los Angeles,'],
                  ['The Green Bay Packers...Green Bay, Wisconsin.', 'The Packers compete...Football Conference']],
