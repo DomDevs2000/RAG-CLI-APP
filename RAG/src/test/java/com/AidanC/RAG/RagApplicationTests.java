c4b10975dde7c41d426b4e12d143fcf17bdb9bf7
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2024-08-15 23:00:24 +0100

    updated tests

--- RAG/src/test/java/com/AidanC/RAG/RagApplicationTests.java
+++ RAG/src/test/java/com/AidanC/RAG/RagApplicationTests.java
@@ -49,39 +49,38 @@ public class RagApplicationTests {
   @Value("classpath:/docs/Apple_AnnualReport_2023.pdf")
   private Resource pdfResource;
 
-  @BeforeEach
-  void beforeTest() throws InterruptedException {
-    logger.info("TEST STARTED ==================================");
-  }
-
+  // @BeforeEach
+  // void beforeTest() throws InterruptedException {
+  // logger.info("TEST STARTED ==================================");
+  // }
+  //
   @AfterEach
   void afterTest() throws InterruptedException {
-    logger.info("TEST ENDED ====================================");
+    // logger.info("TEST ENDED ====================================");
     Thread.sleep(60000);
   }
 
   //
-  @Test
-  @Disabled
-  void whenQueryAskedWithinContext_thenAnswerFromTheContext() {
-    var response = ragService.getAnswer("What was Nvidia's 2023 Total Revenue?");
-    assertNotNull(response);
-    logger.info("Response from RAG LLM: {}", response);
-  }
+  // @Test
+  // @Disabled
+  // void whenQueryAskedWithinContext_thenAnswerFromTheContext() {
+  // var response = ragService.getAnswer("What was Nvidia's 2023 Total Revenue?");
+  // assertNotNull(response);
+  // logger.info("Response from RAG LLM: {}", response);
+  // }
+  //
+  // @Test
+  // @Disabled
+  // void whenQueryAskedOutOfContext_thenDontAnswer() {
+  // var response = ragService.getAnswer("Why is the sky black?");
+  // assertEquals("I don't know the answer.", response);
+  // logger.info("Response from RAG LLM: {}", response);
+  // }
 
   @Test
-  @Disabled
-  void whenQueryAskedOutOfContext_thenDontAnswer() {
-    var response = ragService.getAnswer("Why is the sky black?");
-    assertEquals("I don't know the answer.", response);
-    logger.info("Response from RAG LLM: {}", response);
-  }
-
-  @Test
-  @RepeatedTest(5)
+  @RepeatedTest(4)
   void testEvaluation() throws InterruptedException {
-    String query = "Summarise Apple's 5 year cumulative return";
-
+    String query = "How long is Apple's Fiscal Year";
     ChatResponse response = ChatClient.builder(openAiChatModel)
         .build().prompt()
         .advisors(new QuestionAnswerAdvisor(vectorStore, SearchRequest.query(query).withTopK(3)))
@@ -96,117 +95,133 @@ public class RagApplicationTests {
 
     EvaluationResponse evaluationResponse = relevancyEvaluator.evaluate(evaluationRequest);
 
-    logger.info("Evaluation Response from RAG LLM: {}", evaluationResponse);
+    // logger.info("Evaluation Response from RAG LLM: {}", evaluationResponse);
     logger.info("Evaluation Response Score: {}", evaluationResponse.getScore());
     assertTrue(evaluationResponse.isPass(), "Response is not relevant to the question");
 
   }
 
-  @Test
-  @Disabled
-  @RepeatedTest(5)
-  void testEvaluation2() throws InterruptedException {
-    String query = "How long is Apple's fiscal period";
-
-    ChatResponse response = ChatClient.builder(openAiChatModel)
-        .build().prompt()
-        .advisors(new QuestionAnswerAdvisor(vectorStore,
-            SearchRequest.query(query).withTopK(3)))
-        .user(query)
-        .call()
-        .chatResponse();
-
-    var relevancyEvaluator = new RelevancyEvaluator(ChatClient.builder(openAiChatModel));
-
-    EvaluationRequest evaluationRequest = new EvaluationRequest(query,
-        (List<Content>) response.getMetadata().get(QuestionAnswerAdvisor.RETRIEVED_DOCUMENTS),
-        response);
-    EvaluationResponse evaluationResponse = relevancyEvaluator.evaluate(evaluationRequest);
-
-    logger.info("Chat Response from RAG LLM: {}", response);
-    logger.info("Evaluation Response from RAG LLM: {}", evaluationResponse);
-    assertTrue(evaluationResponse.isPass(), "Response is not relevant to the question");
-
-  }
-
-  @Test
-  @Disabled
-  @RepeatedTest(5)
-  void testEvaluation3() throws InterruptedException {
-    String query = "What macro economic conditions affected apple";
-
-    ChatResponse response = ChatClient.builder(openAiChatModel)
-        .build().prompt()
-        .advisors(new QuestionAnswerAdvisor(vectorStore,
-            SearchRequest.query(query).withTopK(3)))
-        .user(query)
-        .call()
-        .chatResponse();
-
-    var relevancyEvaluator = new RelevancyEvaluator(ChatClient.builder(openAiChatModel));
-
-    EvaluationRequest evaluationRequest = new EvaluationRequest(query,
-        (List<Content>) response.getMetadata().get(QuestionAnswerAdvisor.RETRIEVED_DOCUMENTS),
-        response);
-    EvaluationResponse evaluationResponse = relevancyEvaluator.evaluate(evaluationRequest);
-
-    logger.info("Chat Response from RAG LLM: {}", response);
-    logger.info("Evaluation Response from RAG LLM: {}", evaluationResponse);
-    assertTrue(evaluationResponse.isPass(), "Response is not relevant to the question");
-
-  }
-
-  @Test
-  @Disabled
-  @RepeatedTest(5)
-  void testEvaluation4() throws InterruptedException {
-    String query = "How long is Apple's fiscal period";
-
-    ChatResponse response = ChatClient.builder(openAiChatModel)
-        .build().prompt()
-        .advisors(new QuestionAnswerAdvisor(vectorStore,
-            SearchRequest.query(query).withTopK(3)))
-        .user(query)
-        .call()
-        .chatResponse();
-
-    var relevancyEvaluator = new RelevancyEvaluator(ChatClient.builder(openAiChatModel));
-
-    EvaluationRequest evaluationRequest = new EvaluationRequest(query,
-        (List<Content>) response.getMetadata().get(QuestionAnswerAdvisor.RETRIEVED_DOCUMENTS),
-        response);
-    EvaluationResponse evaluationResponse = relevancyEvaluator.evaluate(evaluationRequest);
-
-    logger.info("Chat Response from RAG LLM: {}", response);
-    logger.info("Evaluation Response from RAG LLM: {}", evaluationResponse);
-    assertTrue(evaluationResponse.isPass(), "Response is not relevant to the question");
-
-  }
-
-  @Test
-  @Disabled
-  @RepeatedTest(5)
-  void testEvaluation5() throws InterruptedException {
-    String query = "How long is Apple's fiscal period";
-
-    ChatResponse response = ChatClient.builder(openAiChatModel)
-        .build().prompt()
-        .advisors(new QuestionAnswerAdvisor(vectorStore,
-            SearchRequest.query(query).withTopK(3)))
-        .user(query)
-        .call()
-        .chatResponse();
-
-    var relevancyEvaluator = new RelevancyEvaluator(ChatClient.builder(openAiChatModel));
-
-    EvaluationRequest evaluationRequest = new EvaluationRequest(query,
-        (List<Content>) response.getMetadata().get(QuestionAnswerAdvisor.RETRIEVED_DOCUMENTS),
-        response);
-    EvaluationResponse evaluationResponse = relevancyEvaluator.evaluate(evaluationRequest);
-
-    logger.info("Chat Response from RAG LLM: {}", response);
-    logger.info("Evaluation Response from RAG LLM: {}", evaluationResponse);
-    assertTrue(evaluationResponse.isPass(), "Response is not relevant to the question");
-
-  }
+  // @Test
+  // @Disabled
+  // @RepeatedTest(5)
+  // void testEvaluation2() throws InterruptedException {
+  // String query = "How long is Apple's fiscal period";
+  //
+  // ChatResponse response = ChatClient.builder(openAiChatModel)
+  // .build().prompt()
+  // .advisors(new QuestionAnswerAdvisor(vectorStore,
+  // SearchRequest.query(query).withTopK(3)))
+  // .user(query)
+  // .call()
+  // .chatResponse();
+  //
+  // var relevancyEvaluator = new
+  // RelevancyEvaluator(ChatClient.builder(openAiChatModel));
+  //
+  // EvaluationRequest evaluationRequest = new EvaluationRequest(query,
+  // (List<Content>)
+  // response.getMetadata().get(QuestionAnswerAdvisor.RETRIEVED_DOCUMENTS),
+  // response);
+  // EvaluationResponse evaluationResponse =
+  // relevancyEvaluator.evaluate(evaluationRequest);
+  //
+  // logger.info("Chat Response from RAG LLM: {}", response);
+  // logger.info("Evaluation Response from RAG LLM: {}", evaluationResponse);
+  // assertTrue(evaluationResponse.isPass(), "Response is not relevant to the
+  // question");
+  //
+  // }
+  //
+  // @Test
+  // @Disabled
+  // @RepeatedTest(5)
+  // void testEvaluation3() throws InterruptedException {
+  // String query = "Summarise the economic factors that affected apple";
+  //
+  // ChatResponse response = ChatClient.builder(openAiChatModel)
+  // .build().prompt()
+  // .advisors(new QuestionAnswerAdvisor(vectorStore,
+  // SearchRequest.query(query).withTopK(3)))
+  // .user(query)
+  // .call()
+  // .chatResponse();
+  //
+  // var relevancyEvaluator = new
+  // RelevancyEvaluator(ChatClient.builder(openAiChatModel));
+  //
+  // EvaluationRequest evaluationRequest = new EvaluationRequest(query,
+  // (List<Content>)
+  // response.getMetadata().get(QuestionAnswerAdvisor.RETRIEVED_DOCUMENTS),
+  // response);
+  // EvaluationResponse evaluationResponse =
+  // relevancyEvaluator.evaluate(evaluationRequest);
+  //
+  // logger.info("Chat Response from RAG LLM: {}", response);
+  // logger.info("Evaluation Response from RAG LLM: {}", evaluationResponse);
+  // assertTrue(evaluationResponse.isPass(), "Response is not relevant to the
+  // question");
+  //
+  // }
+  //
+  // @Test
+  // @Disabled
+  // @RepeatedTest(5)
+  // void testEvaluation4() throws InterruptedException {
+  // String query = "How long is Apple's fiscal period";
+  //
+  // ChatResponse response = ChatClient.builder(openAiChatModel)
+  // .build().prompt()
+  // .advisors(new QuestionAnswerAdvisor(vectorStore,
+  // SearchRequest.query(query).withTopK(3)))
+  // .user(query)
+  // .call()
+  // .chatResponse();
+  //
+  // var relevancyEvaluator = new
+  // RelevancyEvaluator(ChatClient.builder(openAiChatModel));
+  //
+  // EvaluationRequest evaluationRequest = new EvaluationRequest(query,
+  // (List<Content>)
+  // response.getMetadata().get(QuestionAnswerAdvisor.RETRIEVED_DOCUMENTS),
+  // response);
+  // EvaluationResponse evaluationResponse =
+  // relevancyEvaluator.evaluate(evaluationRequest);
+  //
+  // logger.info("Chat Response from RAG LLM: {}", response);
+  // logger.info("Evaluation Response from RAG LLM: {}", evaluationResponse);
+  // assertTrue(evaluationResponse.isPass(), "Response is not relevant to the
+  // question");
+  //
+  // }
+  //
+  // @Test
+  // @Disabled
+  // @RepeatedTest(5)
+  // void testEvaluation5() throws InterruptedException {
+  // String query = "How long is Apple's fiscal period";
+  //
+  // ChatResponse response = ChatClient.builder(openAiChatModel)
+  // .build().prompt()
+  // .advisors(new QuestionAnswerAdvisor(vectorStore,
+  // SearchRequest.query(query).withTopK(3)))
+  // .user(query)
+  // .call()
+  // .chatResponse();
+  //
+  // var relevancyEvaluator = new
+  // RelevancyEvaluator(ChatClient.builder(openAiChatModel));
+  //
+  // EvaluationRequest evaluationRequest = new EvaluationRequest(query,
+  // (List<Content>)
+  // response.getMetadata().get(QuestionAnswerAdvisor.RETRIEVED_DOCUMENTS),
+  // response);
+  // EvaluationResponse evaluationResponse =
+  // relevancyEvaluator.evaluate(evaluationRequest);
+  //
+  // logger.info("Chat Response from RAG LLM: {}", response);
+  // logger.info("Evaluation Response from RAG LLM: {}", evaluationResponse);
+  // assertTrue(evaluationResponse.isPass(), "Response is not relevant to the
+  // question");
+  //
+  // }
 }
