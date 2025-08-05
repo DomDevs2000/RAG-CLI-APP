88df78b6ecfd303622dd595f0c0ea14de7e3ecb5
Aidan Dominic Carvalho <aidan@carvalho.org.uk>
2025-08-05 01:43:25 +0100

    fix CLI test from hanging

--- CLI/src/test/java/com/AidanC/CLI/CliApplicationTests.java
+++ CLI/src/test/java/com/AidanC/CLI/CliApplicationTests.java
@@ -2,8 +2,13 @@ package com.AidanC.CLI;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.shell.interactive.enabled=false",
+    "spring.main.web-application-type=none"
+})
 class CliApplicationTests {
 
 	@Test
